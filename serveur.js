f61d1eb9bc98a7a648a5a7b6e532f1cec842c1fd
Clement Boquet <boquet.clement76@gmail.com>
2021-01-12 13:15:46 +0100

    test1

--- serveur.js
+++ serveur.js
@@ -1,6 +1,18 @@
 let express = require ('express');
 let app = express();
 let port = 3000;
+let  bodyparser = require ('body-parser');
+
+app.set ('view engine' , 'ejs');
+app.use(express.static(__dirname + '/www'));
+app.use( '/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use( '/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use( '/js', express.static(__dirname + '/node_modules/popper.js/dist/umd'));
+app.use( '/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/views', express.static(__dirname + '/views'));
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
 
 app.listen (port, () => {
     console.log('Le Serveur est en route');
@@ -8,6 +20,32 @@ app.listen (port, () => {
 
 })
 
+let monObjet = {
+ nom: "monObjet",
+ valeur: 10
+}
+
+//Route
+
 app.get('/', (req, res, next)=> {
-    res.send('Bonjour !');
+    //res.sendFile('www/index.html');
+    res.render('index.ejs', {monObjet: monObjet});
 });
+
+app.get('/page2', (req, res, next)=> {
+    //res.sendFile('www/index.html');
+    res.render('page2.ejs');
+});
+
+app.post('/page2', (req, res, next)=> {
+    console.log(req.body.name);
+
+});
+
+
+
+app.use((req, res, next)=> {
+   res.status(404).render('error.ejs');
+});
+
+
